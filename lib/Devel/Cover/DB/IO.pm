d766c2c91cfded14cd12b2d94c057fc9e109930d
Audrey Tang <audreyt@audreyt.org>
2012-03-27 13:48:30 +0800

    * Change JSON::PP dependency to JSON so systems with JSON::XS can run much faster.
      This is especially relevant in the merge-runs stage.

--- lib/Devel/Cover/DB/IO.pm
+++ lib/Devel/Cover/DB/IO.pm
@@ -16,11 +16,11 @@ my $Format;
 
 BEGIN
 {
-    $Format = "Storable" if eval "use Storable; 1";
+    $Format = "JSON"     if eval "use JSON; 1";
+    # warn "JSON available\n" if $INC{"JSON.pm"};
+    $Format = "Storable" if !$Format and eval "use Storable; 1";
     # warn "Storable available\n" if $INC{"Storable.pm"};
-    $Format = "JSON"     if eval "use JSON::PP; 1";
-    # warn "JSON::PP available\n" if $INC{"JSON/PP.pm"};
-    die "Can't load either JSON::PP or Storable" unless $Format;
+    die "Can't load either JSON or Storable" unless $Format;
 }
 
 sub new
