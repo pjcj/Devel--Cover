cc9924c7341d92ffd1dc9c57781d20dcc71ce4bd
Paul Johnson <paul@pjcj.net>
2012-05-22 18:25:45 +0200

    Improve example bisect code.

--- utils/bisect.sh
+++ utils/bisect.sh
@@ -1,12 +1,20 @@
+#!/bin/sh -x
+
+make
+make
 make clean
+cp /tmp/accessor /tmp/Accessor_maker.pm tests
+cp /tmp/accessor.5.008 test_output/cover
 perl Makefile.PL
 make
-make test TEST_FILES=t/e2e/aaccessor.t
-
+t=t/e2e/aaccessor.t
+[ ! -e $t ] && t=t/aaccessor.t
+make test TEST_FILES=$t
 
 ret=$?
 [ $ret -gt 127 ] && ret=127
 
+rm tests/accessor tests/Accessor_maker.pm test_output/cover/accessor.5.008
 
 # exit $ret
 
