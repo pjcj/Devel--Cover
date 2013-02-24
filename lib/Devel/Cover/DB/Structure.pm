570c83d0d45dcbdab98f08d5fc9d5d4187066bb0
Paul Johnson <paul@pjcj.net>
2013-02-24 17:47:23 +0100

    Fix "uninitialized value $digest" warning.

--- lib/Devel/Cover/DB/Structure.pm
+++ lib/Devel/Cover/DB/Structure.pm
@@ -280,7 +280,7 @@ sub write
     {
         $self->{f}{$file}{file} = $file;
         my $digest = $self->{f}{$file}{digest};
-        $digest = $1 if $digest =~ /(.*)/; # ie tainting.
+        $digest = $1 if $digest && $digest =~ /(.*)/; # ie tainting.
         unless ($digest)
         {
             warn "Can't find digest for $file"
