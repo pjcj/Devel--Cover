963e3dc3e3c861f0892ecd881676279c69627afd
Paul Johnson <paul@pjcj.net>
2025-07-20 23:10:36 +0200

    Enhance compilation report documentation

--- lib/Devel/Cover/Report/Compilation.pm
+++ lib/Devel/Cover/Report/Compilation.pm
@@ -150,8 +150,120 @@ This module provides a textual reporting mechanism for coverage data. It is
 designed to be called from the C<cover> program.
 
 It produces one report per line, in a format like Perl's own compilation error
-messages.  This makes it easy to, e.g. use Emacs compilation mode to step
-through the reports.
+messages. This makes it easy to use with development tools that understand
+compilation output formats, such as Emacs compilation mode, vim quickfix,
+or IDE error navigation features.
+
+=head1 OUTPUT FORMAT
+
+The compilation report generates output in the following formats:
+
+=over 4
+
+=item * Statements
+
+  Uncovered statement at filename.pm line 42:
+
+=item * Branches
+
+  Branch never true at filename.pm line 15: condition
+  Branch never false at filename.pm line 20: unless condition
+  Branch never reached at filename.pm line 25: condition
+
+=item * Conditions
+
+  Uncovered condition (left, right) at filename.pm line 30: expr && expr
+
+=item * Subroutines
+
+  Uncovered subroutine function_name at filename.pm line 50
+
+=item * POD Documentation
+
+  Uncovered pod at filename.pm line 60
+
+=back
+
+=head1 USAGE WITH DEVELOPMENT TOOLS
+
+=head2 Emacs Compilation Mode
+
+To use with Emacs compilation mode:
+
+  M-x compile
+  cover -report compilation
+
+Then use C-x ` (next-error) to jump to each uncovered location.
+
+=head2 Vim Quickfix
+
+To use with vim quickfix:
+
+  :cgetexpr system('cover -report compilation')
+  :copen
+
+Then use :cn and :cp to navigate between uncovered locations.
+
+=head1 COVERAGE TYPES
+
+The compilation report supports all standard Devel::Cover coverage types:
+
+=over 4
+
+=item * B<statement> - Individual Perl statements
+
+=item * B<branch> - Conditional branch execution paths
+
+=item * B<condition> - Boolean condition combinations
+
+=item * B<subroutine> - Subroutine call coverage
+
+=item * B<pod> - POD documentation coverage
+
+=back
+
+Use the standard C<cover> command options to select which types to report:
+
+  cover -report compilation +statement +branch +condition +subroutine +pod
+
+=head1 FUNCTIONS
+
+=head2 print_statement($db, $file, $options)
+
+Prints uncovered statement coverage information for the specified file.
+Outputs one line per uncovered statement in the format:
+
+  "Uncovered statement at $file line $line_number:"
+
+=head2 print_branches($db, $file, $options)
+
+Prints uncovered branch coverage information for the specified file.
+Reports branches where one or both execution paths were not taken.
+Outputs detailed information about which branch condition was never
+true, false, or reached.
+
+=head2 print_conditions($db, $file, $options)
+
+Prints uncovered condition coverage information for the specified file.
+Reports logical conditions that were not fully exercised, showing
+which parts of complex boolean expressions were not tested.
+
+=head2 print_subroutines($db, $file, $options)
+
+Prints uncovered subroutine coverage information for the specified file.
+Reports subroutines that were never called during testing.
+
+=head2 print_pod($db, $file, $options)
+
+Prints uncovered POD (Plain Old Documentation) coverage information
+for the specified file. Reports sections of POD documentation that
+do not have corresponding tested code.
+
+=head2 report($pkg, $db, $options)
+
+Main entry point for generating compilation-style coverage reports.
+Iterates through all files and calls the appropriate print functions
+based on the coverage types requested in the options.
 
 =head1 SEE ALSO
 
