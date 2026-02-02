c0bb749466fe9b697da830acfda9b314fd9750c8
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-02-01 21:26:38 -0500

    spelling: led
    Ticket GH-368
    
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- lib/Devel/Cover.pm
+++ lib/Devel/Cover.pm
@@ -1453,7 +1453,7 @@ The latest version of Devel::Cover on which Perl 5.12 to 5.18 was supported was
 1.38.  The latest version of Devel::Cover on which Perl 5.8 was supported was
 1.23.  Perl versions 5.6.1 and 5.6.2 were not supported after version 1.22.
 Perl versions 5.6.0 and earlier were never supported.  Using Devel::Cover with
-Perl 5.8.7 was always problematic and frequently lead to crashes.
+Perl 5.8.7 was always problematic and frequently led to crashes.
 
 Different versions of perl may give slightly different results due to changes
 in the op tree.
