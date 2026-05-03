40a09d02be77f7114125a7569421e966fdeee980
Paul Johnson <paul@pjcj.net>
2026-05-03 21:48:58 +0200

    Add POD stubs for Mcdc and Mcdc::Analyser
    Ticket GH-478
    
    - Define the public API: criterion class structure modelled on `Branch`,
      analyser entry point `Devel::Cover::Mcdc::Analyser->analyse($table)`
      returning `{ total, satisfied, pairs, missing }`.

---
+++ lib/Devel/Cover/Mcdc.pm
@@ -0,0 +1,66 @@
+# Copyright 2026, Paul Johnson (paul@pjcj.net)
+
+# This software is free.  It is licensed under the same terms as Perl itself.
+
+# The latest version of this software should be available from my homepage:
+# https://pjcj.net
+
+package Devel::Cover::Mcdc;
+
+use 5.20.0;
+use warnings;
+use feature qw( postderef signatures );
+no warnings qw( experimental::postderef experimental::signatures );
+
+# VERSION
+
+"
+The answer, my friend, is blowin' in the wind
+The answer is blowin' in the wind
+"
+
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Devel::Cover::Mcdc - Modified Condition/Decision Coverage criterion
+
+=head1 SYNOPSIS
+
+ use Devel::Cover::Mcdc;
+
+=head1 DESCRIPTION
+
+Criterion class for Modified Condition/Decision Coverage (MC/DC). Aggregates
+per-decision MC/DC results across files and exposes the standard
+L<Devel::Cover::Criterion> interface (C<total>, C<covered>, C<percentage>,
+C<error>) for reporting.
+
+MC/DC is a parallel criterion alongside C<condition>.  It derives from the
+condition truth tables already collected by the existing runtime
+instrumentation, so no new XS data collection is required.
+
+See L<Devel::Cover::Tutorial/2.5 Modified condition/decision coverage> for an
+introduction to the metric and a worked example, and
+L<Devel::Cover::Mcdc::Analyser> for the per-decision analyser this class wraps.
+
+=head1 SEE ALSO
+
+ Devel::Cover::Criterion
+ Devel::Cover::Mcdc::Analyser
+ Devel::Cover::Condition_table
+
+=head1 METHODS
+
+=head1 LICENCE
+
+Copyright 2026, Paul Johnson (paul@pjcj.net)
+
+This software is free.  It is licensed under the same terms as Perl itself.
+
+The latest version of this software should be available from my homepage:
+https://pjcj.net
+
+=cut
